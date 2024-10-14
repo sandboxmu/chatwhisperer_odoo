a07cdcca359268ba35532cfc4fcd7de8771807ba
Olivier Mamet <53740475+sandboxmu@users.noreply.github.com>
2024-10-14 15:20:48 +0400

    Add files via upload

---
+++ odl_website_chatwhisperer_integeration-main/15.0/odl_website_chatwhisperer_integeration/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ChatWhisperer Website Integeration',
+    'version': '16.0.1.0',
+    'summary': """ChatWhisperer Website Integeration
+    Get your chatbot Iframe from https://bot.chatwhisperer.ai/dashboard/chatbot and add it in Chatbot settings for smooth chatbot integeration
+    """,
+    'author': 'Odolution',
+    'website': 'www.odolution.com',
+    'category': 'Technical',
+    'depends': ['base', 'website'],
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/web_layout_views.xml"
+    ],
+    'images': ['static/description/icon.png'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
