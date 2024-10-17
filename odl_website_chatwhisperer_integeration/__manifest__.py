c917668e5b2df1aba8e6da0b029756a5060b98a5
Usama Shahid <itisusamashahid@gmail.com>
2024-10-17 19:21:17 +0500

    added description
    added description

--- odl_website_chatwhisperer_integeration/__manifest__.py
+++ odl_website_chatwhisperer_integeration/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'ChatWhisperer Website Integeration',
-    'version': '16.0.1.0',
+    'name': 'Chat Whisperer - Website Ai Chatbot',
+    'version': '15.0.1.1',
     'summary': """ChatWhisperer Website Integeration
     Get your chatbot Iframe from https://bot.chatwhisperer.ai/dashboard/chatbot and add it in Chatbot settings for smooth chatbot integeration
     """,
